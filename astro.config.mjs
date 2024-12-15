78b332712b6c6dad6e12b451fe7cdf8c3c28a56c
Pablo Zuval <pablo.zuniga.vale@gmail.com>
2024-12-14 23:58:29 -0300

    🚀 First version portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://pablozuval.github.io/",
+  base: "/",
+  // trailingSlash:"always",
   integrations: [mdx(), sitemap(), tailwind()],
 });
